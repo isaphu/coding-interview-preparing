ec90ff1a34f20eeecc0dde16a6e44b991a3396fd
Isa <uronohan@gmail.com>
2021-02-13 08:14:12 +0000

    adding index

--- big-o.js
+++
@@ -1,9 +0,0 @@
-const nemo = ['nemo'];
-
-export function findNemo(array) {
-  for(let i = 0; i < array.length; i++) {
-    if(array[i] === 'nemo') {
-      console.log('Nemo found!')
-    }
-  }
-}
